b9b4c404c8dbb9b732abf7b66975b803375bb668
Dron Rathore <dron.rathore@gmail.com>
2017-08-08 02:17:19 +0530

    Update docs

--- src/views/docsView.js
+++ src/views/docsView.js
@@ -95,6 +95,26 @@ class DocsView extends React.Component {
           <pre className="dont-wrap">
             <span className="yellow">curl</span> https://safedelivr.com/api/batch/<span className="orange">8020fcbf-7a31-11e7-9cb0-a45e60d46cc1</span>?user_id=YOUR_USER_ID&api_key=API_KEY
           </pre>
+          <p>The API response will be in the format as shown below</p>
+          <pre>
+            {"{"}<br/>
+              &emsp;&emsp;"code": "",<br/>
+              &emsp;&emsp;"created_at": "2017-08-07T12:01:59.985Z",<br/>
+              &emsp;&emsp;"description": "",<br/>
+              &emsp;&emsp;"options": {"{"}<br/>
+                  &emsp;&emsp;&emsp;&emsp;"content": "\u003cp\u003eHi\u003c/p\u003e \u003cp\u003eWelcome to SafeDelivr, please go to the dashboard to access stuff\u003c/p\u003e",<br/>
+                  &emsp;&emsp;&emsp;&emsp;"from": "no-reply@safedelivr.com",<br/>
+                  &emsp;&emsp;&emsp;&emsp;"isBulk": "true",<br/>
+                  &emsp;&emsp;&emsp;&emsp;"name": "Dron Rathore",<br/>
+                  &emsp;&emsp;&emsp;&emsp;"subject": "Welcome to SafeDelivr",<br/>
+                  &emsp;&emsp;&emsp;&emsp;"to": "dron.rathore@gmail.com,dron.rathore@housing.com"<br/>
+              },<br/>
+              &emsp;&emsp;"reason": "",<br/>
+              &emsp;&emsp;"status": "queued",<br/>
+              &emsp;&emsp;"subject": "Welcome to SafeDelivr",<br/>
+              &emsp;&emsp;"user_id": "8020fcbf-7a31-11e7-9cb0-a45e60d46cc1"<br/>
+          }
+          </pre>
           <p>
             The above will return the a JSON struct with the details of the batch and its status, it contains the original message too under the options key.
           </p>
@@ -106,7 +126,38 @@ class DocsView extends React.Component {
           <div className="heading secondary">
             Accessing Logs
           </div>
-          <p>This feature is currently closed, logs can be accessed after logging into your account under <b>Logs</b> tab. Though this isn't a big stuff, I will try to push this feature soon.</p>
+          <p>To access logs of a <b>batch_id</b> you can hit the below endpoint</p>
+          <pre>
+            /api/batch/batch-id/logs?user_id=user_id&api_key=api_key
+          </pre>
+          <p>Above request will return a response similar to this</p>
+          <pre>
+            [{"{"}"<br/>
+                &emsp;&emsp;"batch_id": "37ba28f3-7b68-11e7-a84f-a45e60d46cc1",<br/>
+                &emsp;&emsp;"created_at": "2017-08-07T13:29:17.933Z",<br/>
+                &emsp;&emsp;"email": "alice@example.com",<br/>
+                &emsp;&emsp;"last_update": "2017-08-07T13:29:18.439Z",<br/>
+                &emsp;&emsp;"log_id": "69c9952e-7b74-11e7-8ac6-a45e60d46cc1",<br/>
+                &emsp;&emsp;"state": "dispatched",<br/>
+                &emsp;&emsp;"status": {"{"}<br/>
+                    &emsp;&emsp;&emsp;&emsp;"mailgun": true,<br/>
+                    &emsp;&emsp;&emsp;&emsp;"sendgrid": true<br/>
+                &emsp;&emsp;},<br/>
+                &emsp;&emsp;"user_id": "8020fcbf-7a31-11e7-9cb0-a45e60d46cc1"<br/>
+            }, {"{"}<br/>
+                &emsp;&emsp;"batch_id": "37ba28f3-7b68-11e7-a84f-a45e60d46cc1",<br/>
+                &emsp;&emsp;"created_at": "2017-08-07T13:25:20.291Z",<br/>
+                &emsp;&emsp;"email": "dron.rathore@sendgrid.com",<br/>
+                &emsp;&emsp;"last_update": "2017-08-07T13:25:21.73Z",<br/>
+                &emsp;&emsp;"log_id": "dc2469d5-7b73-11e7-9ba6-a45e60d46cc1",<br/>
+                &emsp;&emsp;"state": "dispatched",<br/>
+                &emsp;&emsp;"status": {"{"}<br/>
+                    &emsp;&emsp;&emsp;&emsp;"mailgun": true,<br/>
+                    &emsp;&emsp;&emsp;&emsp;"sendgrid": false<br/>
+                &emsp;&emsp;},<br/>
+                &emsp;&emsp;"user_id": "8020fcbf-7a31-11e7-9cb0-a45e60d46cc1"<br/>
+            }]
+          </pre>
           <div className="heading secondary">
             Webhooks
           </div>
@@ -115,7 +166,7 @@ class DocsView extends React.Component {
             Architecture
           </div>
           <p>Safedelivr makes use of RabbitMq queues to transmit emails, whenever you make a request to the API server in most cases it will be accepted and than pushed to the queue.</p>
-          <p>The fail-safe mechanism is incorporated within the <a href="https://github.com/dronrathore/safedelivr/src/worker/generics.go#L132">consumer</a>, whenever a Mail Provider crashes consumer will retry the same request with next availaible mail provider, and if that too fails than the same cycle is repeated till <b>MAX_RETRY_COUNT</b>, after that the request is marked failed.</p>
+          <p>The fail-safe mechanism is incorporated within the <a href="https://github.com/DronRathore/safedelivr/blob/master/src/worker/generics.go#L133-L145">consumer</a>, whenever a Mail Provider crashes consumer will retry the same request with next availaible mail provider, and if that too fails than the same cycle is repeated till <b>MAX_RETRY_COUNT</b>, after that the request is marked failed.</p>
           <p>Safedelivr also retries to send a mail if it recieves a failed status from one of the providers webhook for a address. Safedelivr will push that request into the next availaible provider's queue and will wait for that provider's webhook response(to say), if all the providers are exhausted, the request is marked failed.</p>
           <p><b>Safedelivr will not try to resend an email if it recieves a HARD BOUNCE status from the mail provider</b></p>
           <div className="heading secondary">
@@ -148,7 +199,7 @@ class DocsView extends React.Component {
             <span className="red">func</span> <span className="orange">GetEmailVars</span>(batchOptions <span className="yellow">map[string]string</span>, customArgs <span className="yellow">map[string]string</span>) (body <span className="red">string</span>, err <span className="red">error</span>)<br/>
             <span className="red">func</span> <span className="orange">SendRequest</span>(body <span className="yellow">*string</span>) (statusCode <span className="red">int</span>, err <span className="red">error</span>)
           </pre>
-          <p>To get a gist of what a consumer will look like look at this <a href="https://github.com/dronrathore/safedelivr/src/worker/sendgrid.go">sendgrid integration</a>.</p>
+          <p>To get a gist of what a consumer will look like look at this <a href="https://github.com/dronrathore/safedelivr/blob/master/src/worker/sendgrid.go">sendgrid integration</a>.</p>
           <div className="heading">
             webhook
           </div>
